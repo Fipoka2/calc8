5864f9e9d0727df87b637b3945a71f827afdd3e7
fipoka2 <smnv96@yandex.ru>
2019-03-01 14:15:33 +0300

    sub button

--- src/main.js
+++ src/main.js
@@ -1,39 +1,75 @@
-
 Vue.component('button-x', {
-    props:['x'],
+    props: ['x'],
     template: `<div class="col-4">
                     <button class="btn btn-primary" v-on:click="$emit('insert-number', x)">{{ x }}</button>
                </div>`
 });
+
+const ADD = '+';
+const SUB = '-';
 
 const app = new Vue({
     el: '#app',
     data: {
         message: '',
         previousValues: [],
-        sum: ''
-
+        prevOperation: ADD,
+        sum: '',
+        invalid: false
     },
     methods: {
-        decode: function(number) { parseInt(number).toString(8);},
-        encode: function(number) { parseInt(number).toString(10);},
-        addNumber: function(number) { this.message += number.toString();},
-        clear: function() {
+        validate: function (event) {
+            const regExp = new RegExp("^([0-7]|\-)+$");
+            this.invalid = !this.message.match(regExp) || isNaN(parseInt(this.message));
+        },
+        decode: function (number) {
+            parseInt(number).toString(8);
+        },
+        encode: function (number) {
+            parseInt(number).toString(10);
+        },
+        addNumber: function (number) {
+            this.message += number.toString();
+        },
+        clear: function () {
             this.message = '';
             this.previousValues = [];
             this.sum = '';
-            },
-        add: function() {
-           if (this.message) {
-               this.previousValues.push(this.message);
-               this.message = '';
-           }
+            this.prevOperation = ADD;
         },
-        calculate: function() {
-             this.add();
-             let temp = this.previousValues
-                 .reduce( (sum, cur) => sum + parseInt(cur), 0);
-             this.sum = temp.toString(8);
+        add: function () {
+            if (this.message) {
+                this.previousValues.push(
+                    {
+                        o: this.prevOperation,
+                        value: this.message,
+                    });
+                this.prevOperation = ADD;
+                this.message = '';
+            }
+        },
+        sub: function () {
+            if (this.message) {
+                this.previousValues.push(
+                    {
+                        o: this.prevOperation,
+                        value: this.message,
+                    });
+                this.message = '';
+            }
+            this.prevOperation = SUB;
+        },
+        calculate: function () {
+            this.add();
+            let temp = this.previousValues
+                .reduce((sum, cur) => {
+                    if (cur.o === ADD || cur.o == undefined)
+                        return sum + parseInt(cur.value, 8);
+                    else
+                        return sum - parseInt(cur.value, 8);
+
+                }, 0);
+            this.sum = temp.toString(8);
         }
     }
 });
